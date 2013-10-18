0cb8e2afe03e21056d4bfbc0b57f1db66812f8a2
Taheo <taheo2009@onet.pl>
2013-10-18 10:33:02 +0200

    Add partials render v2 [#1]

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -13,6 +13,7 @@ class UnitsController < ApplicationController
 
     redirect_to units_path
   end
+
   def update
     @unit = Unit.find(params[:id])
     if @unit.update_attributes(unit_params)
@@ -22,12 +23,15 @@ class UnitsController < ApplicationController
     end
 
   end
+
   def show
     @unit = Unit.find(params[:id])
   end
+
   def edit
     @unit = Unit.find(params[:id])
   end
+
   def create
     @unit = Unit.new(unit_params)
     if @unit.save
