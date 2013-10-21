cc511a0846a5242e2348758dbb41eb1e563be2e9
Taheo <taheo2009@onet.pl>
2013-10-21 14:29:27 +0200

    add_display_command[#10]

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,4 +2,20 @@ class Unit < ActiveRecord::Base
   validates :name, presence: true, length: {minimum: 6}
   validates :size, presence: true
   belongs_to :unit_type
+
+  def cost
+    result = size * unit_type.cost
+    result += 10 if standar?
+    result += 10 if champion?
+    result += 10 if musician?
+    result
+  end
+
+  def command
+    pusty = []
+    pusty << 'sztandar' if standar?
+    pusty << 'czempion' if champion?
+    pusty << 'muzyk' if musician?
+    pusty.join(', ')
+  end
 end
